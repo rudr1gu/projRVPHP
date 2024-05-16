85fdcecbdcb760b2b23526717fdcbc870883e337
LuanLancioni <luanlancioni12@gmail.com>
2024-05-16 19:45:56 -0300

    first commit

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela Login</title>
+</head>
+<body>
+    <?php require_once "./controller/main.php" ;?>
+    
+</body>
+</html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '440b187dc095eea4c9d7a6c07eadf85e4a974362',
+        'name' => 'admin/telade-login',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'admin/telade-login' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '440b187dc095eea4c9d7a6c07eadf85e4a974362',
+            'dev_requirement' => false,
+        ),
+    ),
+);
